77ee5aa33e743918859ea0078829e705dcd315d3
appfromape <appfromape@gmail.com>
2021-03-17 08:36:14 +0800

    Initial Commit

---
+++ H4CK3R News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4CK3R News
+//
+//  Created by 程式猿 on 2021/3/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
